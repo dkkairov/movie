ee3544e9645561db52ed2b97a2bbb3f5c2941f6c
dkkairov <dk.kairov@gmail.com>
2022-06-16 21:08:05 +0600

    Added movie search

--- lib/widgets/movie_list/movie_list_widget.dart
+++ lib/widgets/movie_list/movie_list_widget.dart
@@ -20,7 +20,7 @@ class MovieListWidget extends StatelessWidget {
             itemCount: model.movies.length,
             itemExtent: 163,
             itemBuilder: (BuildContext context, int index) {
-
+              model.showedMovieAtIndex(index);
               final movie = model.movies[index];
               final posterPath = movie.posterPath;
 
@@ -95,6 +95,7 @@ class MovieListWidget extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.all(10.0),
           child: TextField(
+            onChanged: model.searchMovie,
             decoration: InputDecoration(
               labelText: 'Search',
               filled: true,
